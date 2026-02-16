17d28dee3a3e9ffff9fb923d691f644b23d3c2cd
Guy Davies <guydo6davies@gmail.com>
2026-02-16 14:15:32 +0000

    Changed App to App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function Logout(): JSX.Element {
+  localStorage.clear();
+  return <Navigate to="/login" />;
+}
+
+function RegisterAndLogout(): JSX.Element {
+  localStorage.clear();
+  return <Register />;
+}
+
+function App(): JSX.Element {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
