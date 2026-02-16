e52b3e089581380d0553fa8033d7130e73fb04c7
Guy Davies <guydo6davies@gmail.com>
2026-02-16 13:18:06 +0000

    Initial Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+import api from "../api";
+import Note from "../components/Note"
+import "../styles/Home.css"
+
+function Home() {
+    return <div>HomePage</div>
+}
+
+export default Home;
